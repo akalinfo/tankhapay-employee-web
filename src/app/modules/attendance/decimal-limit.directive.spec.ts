4e4efb94106fca3c53b3122c7d72e1a70068a052
Vinod Maurya <vinod@akalinfosys.com>
2025-07-02 17:44:12 +0530

    delete code

--- src/app/modules/attendance/decimal-limit.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DecimalLimitDirective } from './decimal-limit.directive';
-
-describe('DecimalLimitDirective', () => {
-  it('should create an instance', () => {
-    const directive = new DecimalLimitDirective();
-    expect(directive).toBeTruthy();
-  });
-});
--- src/app/modules/employee/store/filter.actions.ts
+++
@@ -1,9 +0,0 @@
-// filter.actions.ts
-import { createAction, props } from '@ngrx/store';
-
-export const setFilters = createAction(
-  '[Filters] Set Filters',
-  props<{ filters: any }>()
-);
-
-export const clearFilters = createAction('[Filters] Clear Filters');
